9319b964ae03f1ec181fea1a4299beb81159f313
Ermin Dzafic <71074936+ErminDZ@users.noreply.github.com>
2022-02-07 14:07:51 +0100

    Initial commit

---
+++ src/main/java/entity/Point.java
@@ -0,0 +1,56 @@
+package entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Point {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "x")
+    private int x;
+    @Column(name = "y")
+    private int y;
+
+    public Point() {
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
